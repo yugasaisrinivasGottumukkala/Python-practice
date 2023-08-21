0a7e72b23df6ba7dcbf06d2d701aabd5d049e213
yuga sai srinivasu gottumukkala <36449483+yugasaisrinivasGottumukkala@users.noreply.github.com>
2023-08-21 08:34:24 +0530

    Update variables.py

--- Basics/variables.py
+++ Basics/variables.py
@@ -1,1 +1,22 @@
 
+"""
+Python supports different types of variables such as integers,decimals(floats),Text(strings) etc.
+"""
+A = 1 #an integer or whole number
+
+B = 3.14 #a floating point number or decimal number
+
+C = "Yuga Sai Srinivasu Gottumukkala"  #a Strung or Text
+
+'''
+We Use print() method to print contents to the screen.
+type() is a method in python which describes the datatype of the variable.
+'''
+print(A) #prints contents of variablr A to the screeen. i.e, 1
+type(A) #prints type of data that variable A holds. i.e, Integer/Number
+
+print(B) #prints contents of variablr B to the screeen. i.e, 3.14
+type(B) #prints type of data that variable B holds. i.e, Floating point number/Decimal Number
+
+print(C) #prints contents of variablr C to the screeen. i.e, Yuga Sai Srinivasu Gottumukkala
+type(C) #prints type of data that variable C holds. i.e, String/Text
