8d97bf7d0997d3e6b0d93b528bb26bc2bf22fc2d
yuga sai srinivasu gottumukkala <36449483+yugasaisrinivasGottumukkala@users.noreply.github.com>
2023-08-26 19:23:51 +0530

    Create gcd.py

---
+++ Basics/gcd.py
@@ -0,0 +1,16 @@
+#GCD or HCF program in python
+
+#defining GCD function to calculate GCD
+def gcd(m,n):
+  cf=[] # common factors
+  for i in range(1,min(m,n)+1):
+    if m%i==0 and n%i==0:
+      cf.append(i)
+  return(cf[-1])
+
+#read M,N values and assign them to two variables
+a=int(input("enter m value"))
+b=int(input("enter n value"))
+
+#printing GCD value of a,b by calling GCD function
+print("GCD of {} and {} is {}".format(a,b,gcd(a,b)))
